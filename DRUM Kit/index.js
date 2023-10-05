ca868fd7fee65f6bd472eda0acd2047eafdaf90e
yogendrachowdary <yogendrachowdary101@gmail.com>
2023-10-05 23:39:16 +0530

    drum with keys done

--- DRUM Kit/index.js
+++ DRUM Kit/index.js
@@ -1,23 +1,77 @@
 // document.querySelector("button").addEventListener("click",myListenrFunc);
 
 
-// function myListenrFunc(){
-//     alert("I am Clicked!")
-// }
-
-// another way which most people follow
 
 
+//to call the makeSound method based on click
 for( var i=0;i<document.querySelectorAll(".drum").length;i++){
 
 document.querySelectorAll(".drum")[i].addEventListener("click",function (){
 
-    this.style.color="white";
+var val=this.innerHTML;
+
+makeSound(val);
 
 
 });
 
 }
 
-// var audio =new Audio("./sounds/crash.mp3");
-// audio.play();
+
+document.addEventListener("keypress",function (event) {
+
+    makeSound(event.key);
+    
+});
+
+
+
+
+
+//function to make sound based on the letter in val
+function makeSound(val){
+
+    
+switch (val) {
+    case "w":
+    
+var audio =new Audio("./sounds/tom-1.mp3");
+audio.play();
+
+    break;
+
+
+    case "a":
+        var audio =new Audio("./sounds/tom-2.mp3");
+            audio.play();
+    
+    break;
+    case "s":
+        var audio =new Audio("./sounds/tom-3.mp3");
+        audio.play();
+    break;
+    case "d":
+        var audio =new Audio("./sounds/tom-4.mp3");
+            audio.play();
+    
+    break;
+    case "j":
+        var audio =new Audio("./sounds/kick-bass.mp3");
+            audio.play();
+    
+    break;
+    case "k":
+        var audio =new Audio("./sounds/crash.mp3");
+            audio.play();
+    
+    break;
+    case "l":
+        var audio =new Audio("./sounds/snare.mp3");
+            audio.play();
+    
+    break;
+    default:
+    break;
+}
+
+}
